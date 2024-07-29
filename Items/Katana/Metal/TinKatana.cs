e4ad688e27bd3021e8361493694b5e8dc6797520
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2024-07-29 13:55:29 +0900

    Added 2 Katana
    Added
    ・Copper Katana
    ・Tin Katana

--- Items/Katana/Metal/TinKatana.cs
+++ Items/Katana/Metal/TinKatana.cs
@@ -16,13 +16,13 @@ namespace MoreKatana.Items.Katana.Metal
             Item.height = 54;//アイテム判定の縦幅（拾得する際に使用）
 
             Item.crit = 0;//デフォルトで4%クリティカル率をもらえる
-            Item.useTime = 18;//アイテムを使用していると扱われる時間
-            Item.useAnimation = 18;//アイテムのアニメーションを再生する時間
+            Item.useTime = 20;//アイテムを使用していると扱われる時間
+            Item.useAnimation = 20;//アイテムのアニメーションを再生する時間
 
-            Item.damage = 22;//与えるダメージ
-            Item.knockBack = 6;//与えるノックバック
+            Item.damage = 12;//与えるダメージ
+            Item.knockBack = 5.5f;//与えるノックバック
 
-            Item.value = Item.sellPrice(silver: 10);
+            Item.value = Item.sellPrice(silver: 1, copper: 40);
         }
 
         public override void AddRecipes()
